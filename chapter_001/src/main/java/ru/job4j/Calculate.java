20185477b7b31908e810b1fe1c9068aa9c13ec55
deniskaa <koty.denis@gmail.com>
2017-09-28 23:09:49 +0300

    Added JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,16 @@
 package ru.job4j;
+
+/** class Класс для ввывода приветствия
+ *@author deniskaa
+ *@since 28.07.2017
+ *@version 1.0
+ */
 public class Calculate{
+
+  /**
+   *Начало программы
+   * Метод ничего не принемает и не возвращает
+   */
   public static void main(String[] args) {
     System.out.println("Hello world");
   }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *Package for calculate task/
+ *
+ *@author deniskaa (koty.denis@gmail.com)
+ *@version 1.0
+ *@since 28.09.2017
+ */
+ package ru.job4j;
